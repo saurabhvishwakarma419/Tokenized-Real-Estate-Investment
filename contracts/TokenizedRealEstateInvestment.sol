b6d34559327fd22c1307c26eb9979975bd8c7506
saurabhvishwakarma419 <saurvishwakarma040207@gmail.com>
2025-06-11 00:13:33 +0530

    Update TokenizedRealEstateInvestment.sol

--- contracts/TokenizedRealEstateInvestment.sol
+++ contracts/TokenizedRealEstateInvestment.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
