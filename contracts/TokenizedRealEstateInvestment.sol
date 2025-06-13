e61bfb2ec65f700d6ee81659420939a3ec898846
saurabhvishwakarma419 <saurvishwakarma040207@gmail.com>
2025-06-13 11:02:43 +0530

    Update TokenizedRealEstateInvestment.sol

--- contracts/TokenizedRealEstateInvestment.sol
+++ contracts/TokenizedRealEstateInvestment.sol
@@ -23,6 +23,16 @@ contract DynamicNFTMarketplace is ERC721URIStorage, Ownable, ReentrancyGuard {
     event NFTPurchased(uint256 indexed tokenId, address indexed seller, address indexed buyer, uint256 price);
     event NFTEvolved(uint256 indexed tokenId, uint256 newStage);
     event NFTRelisted(uint256 indexed tokenId, uint256 price);
+    event NFTBurned(uint256 indexed tokenId, address indexed owner);    event NFTListed(uint256 indexed tokenId, address indexed seller, uint256 price);
+    event NFTDelisted(uint256 indexed tokenId, address indexed owner);
+    event NFTPurchased(uint256 indexed tokenId, address indexed seller, address indexed buyer, uint256 price);
+    event NFTEvolved(uint256 indexed tokenId, uint256 newStage);
+    event NFTRelisted(uint256 indexed tokenId, uint256 price);
+    event NFTBurned(uint256 indexed tokenId, address indexed owner);    event NFTListed(uint256 indexed tokenId, address indexed seller, uint256 price);
+    event NFTDelisted(uint256 indexed tokenId, address indexed owner);
+    event NFTPurchased(uint256 indexed tokenId, address indexed seller, address indexed buyer, uint256 price);
+    event NFTEvolved(uint256 indexed tokenId, uint256 newStage);
+    event NFTRelisted(uint256 indexed tokenId, uint256 price);
     event NFTBurned(uint256 indexed tokenId, address indexed owner);
 
     constructor() ERC721("DynamicNFT", "DNFT") {}
