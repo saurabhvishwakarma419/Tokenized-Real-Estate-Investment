1a8c84d8d55a5bb1dfc4de9bb2da2703bc4dc34c
saurabhvishwakarma419 <saurvishwakarma040207@gmail.com>
2025-06-18 23:57:08 +0530

    Update TokenizedRealEstateInvestment.sol

--- contracts/TokenizedRealEstateInvestment.sol
+++ contracts/TokenizedRealEstateInvestment.sol
@@ -9,13 +9,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
     mapping(uint256 => uint256) public tokenPrices;
     mapping(uint256 => uint256) public tokenEvolutionStages;
 
-    mapping(uint256 => uint256) public tokenPrices;
-    mapping(uint256 => uint256) public tokenEvolutionStages;
-    mapping(uint256 => mapping(uint256 => string)) public evolutionStageURIs; event NFTListed(uint256 indexed tokenId, address indexed seller, uint256 price);
-    event NFTDelisted(uint256 indexed tokenId, address indexed owner);
-    event NFTPurchased(uint256 indexed tokenId, address indexed seller, address indexed buyer, uint256 price);
-    event NFTEvolved(uint256 indexed tokenId, uint256 newStage);
-    event NFTRelisted(uint256 indexed tokenId, uint256 price);
+
     event NFTBurned(uint256 indexed tokenId, address indexed owner);    event NFTListed(uint256 indexed tokenId, address indexed seller, uint256 price);
     event NFTDelisted(uint256 indexed tokenId, address indexed owner);
     event NFTPurchased(uint256 indexed tokenId, address indexed seller, address indexed buyer, uint256 price);
