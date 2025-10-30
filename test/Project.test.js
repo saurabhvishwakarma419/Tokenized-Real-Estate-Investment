1d37c43e1f27ca9456276f81fe7e59016f71ec1e
Saurabh vishwakarma <saurvishwakarma040207@gmail.com>
2025-10-30 14:19:11 +0530

    Update Project.test.js

--- test/Project.test.js
+++ test/Project.test.js
@@ -1,3 +1,4 @@
+
 // test/Project.test.js
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
@@ -61,6 +62,7 @@ describe("Tokenized Real Estate Investment Platform", function () {
         "New York City",
         "Premium downtown location",
         ethers.parseEther
+
 
 
 
